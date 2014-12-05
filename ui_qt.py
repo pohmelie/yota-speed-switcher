1f4c014324f0c257fa80056d915a9f9c30a4e6f3
pohmelie <multisosnooley@gmail.com>
2014-12-05 16:16:40 +0300

    partial schedule (not working yet :)

--- ui_qt.py
+++ ui_qt.py
@@ -2,10 +2,12 @@ import sys
 import queue
 import json
 import pathlib
+import functools
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from bs4 import BeautifulSoup as bs
 
+from ui_qt_schedule import ScheduleUI
 from yota import yota
 
 
@@ -47,13 +49,16 @@ class YotaUI:
 
     def __init__(self):
 
-        self.ui = uic.loadUi("login.ui")
-        self.ui.login_button.clicked.connect(self.login)
-        self.ui.username_edit.returnPressed.connect(self.login)
-        self.ui.password_edit.returnPressed.connect(self.login)
-        self.ui.setWindowIcon(QtGui.QIcon("logo-yota.png"))
-        self.ui.setWindowTitle("yota-speed-switcher")
-        self.ui.closeEvent = self.ui_close_event
+        self.login_ui = uic.loadUi("login.ui")
+        self.login_ui.login_button.clicked.connect(self.login)
+        self.login_ui.username_edit.returnPressed.connect(self.login)
+        self.login_ui.password_edit.returnPressed.connect(self.login)
+        self.login_ui.setWindowIcon(QtGui.QIcon("logo-yota.png"))
+        self.login_ui.setWindowTitle("yota-speed-switcher")
+        self.hide_on_close(self.login_ui)
+
+        self.schedule_ui = ScheduleUI()
+        self.hide_on_close(self.schedule_ui)
 
         self.options_filename = "options.json"
         self.load_options()
@@ -62,7 +67,6 @@ class YotaUI:
         self.menu = QtWidgets.QMenu()
         self.tray = QtWidgets.QSystemTrayIcon(QtGui.QIcon("logo-yota-gray.png"))
         self.tray.setContextMenu(self.menu)
-        self.tray.activated.connect(lambda: self.menu.exec(QtGui.QCursor.pos()))
         self.tray.show()
         if sys.platform == "linux":
 
@@ -78,16 +82,20 @@ class YotaUI:
         self.refresh_menu()
         if self.options["remember_me"]:
 
-            self.ui.setEnabled(False)
+            self.login_ui.setEnabled(False)
             self.login()
 
-        self.ui.show()
+        self.login_ui.show()
 
 
-    def ui_close_event(self, e):
+    def hide_on_close(self, ui):
 
-        e.ignore()
-        self.ui.hide()
+        def ui_close_event(e):
+
+            e.ignore()
+            ui.hide()
+
+        ui.closeEvent = ui_close_event
 
 
     def load_options(self):
@@ -112,9 +120,9 @@ class YotaUI:
 
     def set_options(self):
 
-        self.ui.username_edit.setText(self.options["username"])
-        self.ui.password_edit.setText(self.options["password"])
-        self.ui.remember_me_edit.setCheckState(QtCore.Qt.Checked if self.options["remember_me"] else QtCore.Qt.Unchecked)
+        self.login_ui.username_edit.setText(self.options["username"])
+        self.login_ui.password_edit.setText(self.options["password"])
+        self.login_ui.remember_me_edit.setCheckState(QtCore.Qt.Checked if self.options["remember_me"] else QtCore.Qt.Unchecked)
 
 
     def save_options(self):
@@ -139,9 +147,9 @@ class YotaUI:
 
     def get_options(self):
 
-        self.options["username"] = self.ui.username_edit.text()
-        self.options["password"] = self.ui.password_edit.text()
-        self.options["remember_me"] = self.ui.remember_me_edit.checkState() == QtCore.Qt.Checked
+        self.options["username"] = self.login_ui.username_edit.text()
+        self.options["password"] = self.login_ui.password_edit.text()
+        self.options["remember_me"] = self.login_ui.remember_me_edit.checkState() == QtCore.Qt.Checked
 
 
     def show_baloon(self, text):
@@ -157,12 +165,13 @@ class YotaUI:
 
     def login(self):
 
-        self.ui.setEnabled(False)
+        self.login_ui.setEnabled(False)
         self.get_options()
         self.save_options()
 
         self.worker = YotaWorker()
         self.worker.result.connect(self.refresh_menu)
+        self.worker.result.connect(self.schedule_ui.set_slider_data)
         self.worker.request(self.options["username"], self.options["password"])
 
 
@@ -170,14 +179,14 @@ class YotaUI:
     def refresh_menu(self, slider_data=None):
 
         self.menu.clear()
-        self.ui.setEnabled(True)
+        self.login_ui.setEnabled(True)
         if isinstance(slider_data, dict) and not slider_data:
 
             self.show_baloon("Can't login")
 
         elif slider_data is not None:
 
-            self.ui.hide()
+            self.login_ui.hide()
             self.tray.setIcon(QtGui.QIcon("logo-yota.png"))
 
             data = next(iter(slider_data.values()))
@@ -196,7 +205,7 @@ class YotaUI:
 
                 desc = str.format(fmt, *map(lambda k: bs(step[k]).text, fields))
                 self.steps[fmt] = step["code"]
-                action = QtWidgets.QAction(desc, self.ui)
+                action = QtWidgets.QAction(desc, self.login_ui)
                 action.setCheckable(True)
                 print(step["code"], step["speedNumber"], step["speedString"])
                 if step["code"] == data["offerCode"]:
@@ -214,6 +223,7 @@ class YotaUI:
             action.triggered.connect(self.SpeedSetter())
             self.actions.append(action)
 
+        self.menu.addAction("Расписание...").triggered.connect(lambda: self.schedule_ui.show())
         self.menu.addAction("Смена пользователя").triggered.connect(self.logout)
         self.menu.addAction("Выход").triggered.connect(sys.exit)
 
@@ -240,7 +250,7 @@ class YotaUI:
         self.remove_options()
         self.refresh_menu()
         self.tray.setIcon(QtGui.QIcon("logo-yota-gray.png"))
-        self.ui.show()
+        self.login_ui.show()
 
 
     def SpeedSetter(self, code=None, action=None):
